b00fe16a0826b7a9d65ae36d42e368b081825916
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-08-16 12:06:49 +0100

    To my great embaressment, m and n got reversed in the autogenerated masking zones. fixed, and the generating code fragment comited

---
+++ Python/Utils/genMasks.py
@@ -0,0 +1,18 @@
+num = 16
+axis = ["x", "y", "m", "n"]
+human = ["Left", "Top", "Right", "Bottom"]
+
+for i in range( num ):
+    for ax, hm in zip( axis, human ):
+        print( """    "maskzone{0:0>2}{1}": CameraTraits( "maskzone{0:0>2}{1}", 0, 0, 4096, int,
+                                 value=None,
+                                 units="Pixels",
+                                 human_name="Mask Zone {0:0>2} {3}",
+                                 desc="Mask {0:0>2} position {2}" ),
+               """.format( i+1, ax, hm, ax.upper() ) )
+        
+reg = 298
+for i in range( num ):
+    for ax in axis:
+        reg += 2
+        print( """        "maskzone{0:0>2}{1}" : ( {2}, 2, True ),""".format( i+1, ax, reg ) )
