7fd421fe9f0058a54ed198a9ab6b369be29b9b88
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-10-30 14:40:08 +0000

    super basic modern style

--- Python/Experiments/openGLviewport.py
+++ Python/Experiments/openGLviewport.py
@@ -1,10 +1,11 @@
+import ctypes
 import sys
 import numpy as np
 
 from PySide2 import QtCore, QtGui, QtWidgets
 
-from OpenGL import GL, GLU, GLUT
-
+from OpenGL import GL
+from OpenGL.GL.shaders import compileShader, compileProgram
 
 class OGLWindow( QtWidgets.QWidget ):
 
@@ -18,20 +19,98 @@ class OGLWindow( QtWidgets.QWidget ):
         self.setWindowTitle( "Testing OpenGL" )
 
 
+class Shaders( object ):
+    vtx_src = """
+    # version 330 core
+
+    in vec3 position ;
+
+    void main() {
+        gl_Position = vec4( position, 1.0 ) ;
+    }
+    """
+    frg_src = """
+    # version 330 core
+
+    out vec4 colour ;
+
+    void main() {
+        colour = vec4( 1.0, 0.0, 0.0, 1.0 ) ;
+    }
+    """
+
+    vtx2_src = """
+    # version 330 core
+
+    in vec3 position ;
+    in vec3 colour ;
+
+    out vec3 fcolour ;
+
+    void main() {
+        fcolour = colour ;
+        gl_Position = vec4( position, 1.0 ) ;
+    }
+    """
+    frg2_src = """
+    # version 330 core
+
+    in  vec3 fcolour ;
+
+    out vec4 colour ;
+
+    void main() {
+        colour = vec4( fcolour, 1.0 ) ;
+    }
+    """
+
 class GLWidget( QtWidgets.QOpenGLWidget ):
 
     def __init__( self, parent=None ):
         super( GLWidget, self ).__init__( parent )
         self.bgcolor = [ 0, 0.1, 0.1, 1 ]
-        self._prepareResources()
+        self.shader_attr_locs = {}
 
     def _prepareResources( self ):
+        # the item
         self.verts = np.asarray( [ -0.5, -0.5, 0.0,
                                     0.5, -0.5, 0.0,
                                     0.0,  0.5, 0.0, ], dtype=np.float32 )
         self.vert_cols = np.eye( 3, dtype=np.float32 )
-        self.rot = 0.1
 
+        # VBOs
+        vbo = GL.glGenBuffers( 1 )
+        GL.glBindBuffer( GL.GL_ARRAY_BUFFER, vbo )
+        GL.glBufferData( GL.GL_ARRAY_BUFFER, self.verts.nbytes, self.verts, GL.GL_STATIC_DRAW )
+
+        #col = GL.glGenBuffers( 1 )
+        #GL.glBindBuffer( GL.GL_ARRAY_BUFFER, col )
+        #GL.glBufferData( GL.GL_ARRAY_BUFFER, self.vert_cols.nbytes, self.vert_cols, GL.GL_STATIC_DRAW )
+
+        #self.buffers = ( vbo, col )
+
+        # something to move it
+        self.rot = 1.0
+
+
+    def _qglShader( self ):
+        self.shader = QtGui.QOpenGLShaderProgram( self.context() )
+
+        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Vertex, Shaders.vtx_src  )
+        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Fragment, Shaders.frg_src )
+
+        program_attrs = ("position")#, "colour")
+
+        for i, name in enumerate( program_attrs ):
+            self.shader_attr_locs[ name ] = i
+            GL.glBindAttribLocation( self.shader.programId(), self.shader_attr_locs[ name ], name )
+
+        self.shader.link()
+        self.shader.bind()
+
+        for name in program_attrs:
+            GL.glEnableVertexAttribArray( self.shader_attr_locs[ name ] )
+            GL.glVertexAttribPointer( self.shader_attr_locs[ name ], 3, GL.GL_FLOAT, GL.GL_FALSE, 0, None )
 
     # Qt funcs -------------------------------------------------------
     def minimumSizeHint(self):
@@ -43,12 +122,12 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
 
     # gl funs --------------------------------------------------------
     def initializeGL( self ):
+        # buffers
+        self._prepareResources()
+        # shaders
+        self._qglShader()
+        # misc
         GL.glClearColor( *self.bgcolor )
-        GL.glEnableClientState( GL.GL_VERTEX_ARRAY )
-        GL.glEnableClientState( GL.GL_COLOR_ARRAY )
-
-        GL.glVertexPointer( 3, GL.GL_FLOAT, 0, self.verts )
-        GL.glColorPointer( 3, GL.GL_FLOAT, 0, self.vert_cols )
 
     def paintGL( self ):
         GL.glClear( GL.GL_COLOR_BUFFER_BIT )
