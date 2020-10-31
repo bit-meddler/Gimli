56e0cfb7a5ff5225e1ae4b6f813e796891bdb65c
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-10-31 11:31:07 +0000

    Hello Quad

--- Python/Experiments/openGLviewport.py
+++ Python/Experiments/openGLviewport.py
@@ -42,8 +42,8 @@ class Shaders( object ):
     vtx2_src = """
     # version 330 core
 
-    in vec3 position ;
-    in vec3 colour ;
+    layout( location=0 ) in vec3 position ;
+    layout( location=1 ) in vec3 colour ;
 
     out vec3 fcolour ;
 
@@ -76,19 +76,20 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
 
     def _prepareResources( self ):
         # the item
-        self.verts = np.asarray( [ -0.5, -0.5, 0.0,
-                                    0.5, -0.5, 0.0,
-                                    0.0,  0.5, 0.0, ], dtype=np.float32 )
-        self.vert_cols = np.eye( 3, dtype=np.float32 ).ravel()
+        self.big_buff = np.asarray( [
+            #  X     Y    Z    R    G    B
+            [ -0.5, -0.5, 0.0, 1.0, 0.0, 0.0 ],
+            [  0.5, -0.5, 0.0, 0.0, 1.0, 0.0 ],
+            [ -0.5,  0.5, 0.0, 0.0, 0.0, 1.0 ],
+            [  0.5,  0.5, 0.0, 1.0, 1.0, 1.0 ]], dtype=np.float32 )
 
-        self.big_buff = np.concatenate( [self.verts, self.vert_cols] )
-
-        self.bb_strides = [ 0, self.verts.nbytes ]
+        self.bb_ln_sz = self.big_buff[0,:].nbytes
+        self.bb_strides = [ 0, self.big_buff[0,:3].nbytes ]
 
         # VBOs
         vbo = GL.glGenBuffers( 1 )
         GL.glBindBuffer( GL.GL_ARRAY_BUFFER, vbo )
-        GL.glBufferData( GL.GL_ARRAY_BUFFER, self.big_buff.nbytes, self.big_buff, GL.GL_STATIC_DRAW )
+        GL.glBufferData( GL.GL_ARRAY_BUFFER, self.big_buff.nbytes, self.big_buff.ravel(), GL.GL_STATIC_DRAW )
 
 
     def _qglShader( self ):
@@ -108,7 +109,12 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
 
         for name, stride in zip( program_attrs, self.bb_strides ):
             GL.glEnableVertexAttribArray( self.shader_attr_locs[ name ] )
-            GL.glVertexAttribPointer( self.shader_attr_locs[ name ], 3, GL.GL_FLOAT, GL.GL_FALSE, 0, ctypes.c_void_p( stride ) )
+            GL.glVertexAttribPointer( self.shader_attr_locs[ name ],
+                                      3,
+                                      GL.GL_FLOAT,
+                                      GL.GL_FALSE,
+                                      self.bb_ln_sz,
+                                      ctypes.c_void_p( stride ) )
 
     # Qt funcs -------------------------------------------------------
     def minimumSizeHint(self):
@@ -130,7 +136,7 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
     def paintGL( self ):
         GL.glClear( GL.GL_COLOR_BUFFER_BIT )
         GL.glRotate( self.rot, 0.0, 0.0, 1.0 )
-        GL.glDrawArrays( GL.GL_TRIANGLES, 0, 3 )
+        GL.glDrawArrays( GL.GL_TRIANGLE_STRIP, 0, 4 )
         self.rot += 1.0
 
     def resizeGL( self, width, height ):
