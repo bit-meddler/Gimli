c8307349250e288d362d5a3f621fc9394004c9b8
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-11-07 14:06:36 +0000

    Can't texture, but starting on Projection models anyway.

--- Python/Experiments/openGLOrthoVP.py
+++ Python/Experiments/openGLOrthoVP.py
@@ -7,7 +7,7 @@ import cv2
 from PySide2 import QtCore, QtGui, QtWidgets
 
 from OpenGL import GL
-
+from OpenGL.GL.shaders import compileShader, compileProgram
 
 class OGLWindow( QtWidgets.QWidget ):
 
@@ -29,15 +29,18 @@ class Shaders( object ):
         layout( location=1 ) in vec3 a_colour ;
         layout( location=2 ) in vec2 a_uvCoord ;
 
-        out vec3 v_colour ;
-        out vec2 v_uvCoord ;
+        out vec3 colour ;
+        out vec2 uvCoord ;
 
-        uniform mat4 u_rotation ;
+        uniform mat4 u_transform ;
+        uniform mat4 u_model ;
+        uniform mat4 u_view ;
+        uniform mat4 u_projection ;
 
         void main() {
-            gl_Position = u_rotation * vec4( a_position, 1.0f ) ;
-            v_colour = a_colour ;
-            v_uvCoord = a_uvCoord ;
+            gl_Position = u_projection * u_view * u_model * u_transform * vec4( a_position, 1.0f ) ;
+            colour = a_colour ;
+            uvCoord = a_uvCoord ;
 
         }
     """
@@ -45,18 +48,17 @@ class Shaders( object ):
     frg_src = """
         # version 330
 
-        in vec3 v_colour ;
-        in vec2 v_uvCoord ;
+        in vec3 colour ;
+        in vec2 uvCoord ;
 
         out vec4 outColor ;
-        uniform sampler2D s_texture ;
+        uniform sampler2D samplerTex ;
 
         void main() {
-          // outColor = texture( s_texture, v_uvCoord ) ; // * vec4( v_colour, 1.0f ) ;
-          outColor = vec4( v_colour, 1.0 ) ;
+          // outColor = texture( samplerTex, uvCoord ) ;
+          outColor = vec4( colour, 1.0 ) ;
         }
     """
-
 
 class Math3D( object ):
 
@@ -64,7 +66,7 @@ class Math3D( object ):
     def genRotMat( axis, angle, degrees=False ):
         angle = np.deg2rad( angle ) if degrees else angle
         axis = axis.upper()
-        ret = np.eye(4)
+        ret = np.eye( 4, dtype=np.float32 )
 
         ca = np.cos( angle )
         sa = np.sin( angle )
@@ -86,6 +88,21 @@ class Math3D( object ):
 
         return ret
 
+    @staticmethod
+    def genPerspProjectionFrust( h_fov, aspect, near_clip, far_clip ):
+        ymax = near_clip * np.tan( np.deg2rad(h_fov) )
+        xmax = ymax * aspect
+        return Math3D.genPerspProjectionPlaines( -xmax, xmax, -ymax, ymax, near_clip, far_clip )
+
+    @staticmethod
+    def genPerspProjectionPlaines( left, right, bottom, top, near, far ):
+        return np.array((
+            (      2.0 * near / (right - left),                              0.0,                              0.0,  0.0 ),
+            (                              0.0,      2.0 * near / (top - bottom),                              0.0,  0.0 ),
+            (  (right + left) / (right - left),  (top + bottom) / (top - bottom),     -(far + near) / (far - near), -1.0 ),
+            (                              0.0,                              0.0, -2.0 * far * near / (far - near),  0.0 ),
+        ), dtype=np.float32 )
+
 class GLWidget( QtWidgets.QOpenGLWidget ):
 
     def __init__( self, parent=None ):
@@ -98,7 +115,9 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
 
         # something to move it
         self.rot = 1.0
-        self.rot_loc = None
+
+        # Guard
+        self.ready = True
 
     def _prepareResources( self ):
         #        positions         colors          texture coords
@@ -162,21 +181,19 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
         texture = GL.glGenTextures( 1 )
         GL.glBindTexture( GL.GL_TEXTURE_2D, texture )
 
-
-        img = cv2.imread( "wood.jpg" , cv2.IMREAD_COLOR )
-        rgb_img = cv2.cvtColor( img, cv2.COLOR_BGR2RGB )
-        i_h, i_w, _ = img.shape
-        
-        GL.glTexImage2D( GL.GL_TEXTURE_2D, 0, GL.GL_RGB, i_w, i_h, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, rgb_img )
-
         # Set the texture wrapping parameters
-        GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE )
-        GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE )
+        GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT )
+        GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT )
 
         # Set texture filtering parameters
         GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR )
         GL.glTexParameteri( GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR )
 
+        img = cv2.imread( "wood.jpg" , cv2.IMREAD_COLOR )
+        i_h, i_w, _ = img.shape
+
+        GL.glTexImage2D( GL.GL_TEXTURE_2D, 0, GL.GL_RGB, i_w, i_h, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img )
+        GL.glEnable( GL.GL_TEXTURE_2D )
 
     def _qglShader( self ):
 
@@ -203,8 +220,11 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
                                       self.line_sz,
                                       ctypes.c_void_p( offset ) )
 
-        self.rot_loc = self.shader.uniformLocation( "u_rotation" )
-
+        self.xform_loc = self.shader.uniformLocation( "u_transform" )
+        self.model_loc = self.shader.uniformLocation( "u_model" )
+        self.view_loc  = self.shader.uniformLocation( "u_view" )
+        self.proj_loc  = self.shader.uniformLocation( "u_projection" )
+        self.shader.release()
 
     # Qt funcs -------------------------------------------------------
     def minimumSizeHint(self):
@@ -216,7 +236,6 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
 
     # gl funs --------------------------------------------------------
     def initializeGL( self ):
-        GL.glEnable( GL.GL_TEXTURE_2D )
         # buffers
         self._prepareResources()
         # shaders
@@ -224,42 +243,64 @@ class GLWidget( QtWidgets.QOpenGLWidget ):
         # misc
         GL.glHint( GL.GL_POINT_SMOOTH_HINT, GL.GL_NICEST )
         GL.glEnable( GL.GL_POINT_SMOOTH )
-        GL.glEnable( GL.GL_DEPTH_TEST )
-        #GL.glEnable( GL.GL_CULL_FACE )
-
-        GL.glEnable( GL.GL_BLEND )
-        GL.glBlendFunc( GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA )
+        #GL.glEnable( GL.GL_DEPTH_TEST )
         GL.glClearColor( *self.bgcolor )
 
         # ticks for redraws
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect( self.update )
-        self.timer.start( 16 ) # approx 60fps
+        self.timer.start( 16 )
+
+        # setup camera matrixes
+        self.model = np.eye( 4, dtype=np.float32 )
+        self.view  = np.eye( 4, dtype=np.float32 )
+        self.projection = np.eye( 4, dtype=np.float32 )
+        self.view[:3,3] = [0., 0., -.4]
+
+        # assemble a projection
+        w, h = self.wh
+        self.projection = Math3D.genPerspProjectionFrust( 35.0, w/h, 0.10, 100.0 )
+        print( self.projection )
+
+        # Load Uniforms
+        self.shader.bind()
+        GL.glUniformMatrix4fv( self.model_loc, 1, GL.GL_TRUE, self.model )
+        GL.glUniformMatrix4fv( self.view_loc,  1, GL.GL_TRUE, self.view )
+        GL.glUniformMatrix4fv( self.proj_loc,  1, GL.GL_TRUE, self.projection )
+        self.shader.release()
+
+        self.ready = True
 
     def paintGL( self ):
         # guard against early drawing
-        if( self.rot_loc is None ):
-            pass
+        if( not self.ready ):
+            return
 
         GL.glClear( GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT )
 
         GL.glViewport( 0, 0, self.wh[0], self.wh[1] )
 
         rot_x = Math3D.genRotMat( "X", self.rot, degrees=True )
-        #rot_y = Math3D.genRotMat( "Y", self.rot+90, degrees=True )
-        stat_y= Math3D.genRotMat( "Y", 17.5, degrees=True )
+        rot_y = Math3D.genRotMat( "Y", self.rot+90, degrees=True )
 
-        GL.glUniformMatrix4fv( self.rot_loc, 1, GL.GL_TRUE, np.dot( stat_y, rot_x ) )
+        self.shader.bind()
+        GL.glUniformMatrix4fv( self.xform_loc, 1, GL.GL_TRUE, np.dot( rot_y, rot_x ) )
 
         GL.glDrawElements( GL.GL_TRIANGLES, self.num_idx, GL.GL_UNSIGNED_SHORT, ctypes.c_void_p( 0 ) )
-
+        self.shader.release()
         self.rot += 1.0
 
     def resizeGL( self, width, height ):
         self.wh = ( width, height )
         GL.glViewport( 0, 0, width, height )
-    
-    
+
+        #self.projection = Math3D.genPerspProjectionFrust( 90.0, width/height, 0.1, 100.0 )
+
+        self.shader.bind()
+        GL.glUniformMatrix4fv( self.proj_loc,  1, GL.GL_TRUE, self.projection )
+        self.shader.release()
+
+
 if( __name__ == "__main__" ):
     app = QtWidgets.QApplication( sys.argv )
 
