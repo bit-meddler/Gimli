3009682c967bbd1b5034eca674cf10c9f7dd454c
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-11-08 10:24:06 +0000

    Rolled Orthopgraphic navigation into cam viewer. Not working. do I need an EBO of P mats rather than uploading per draw?

---
+++ Python/Core/math3D.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+FLOAT_T = np.float32
+
+def genRotMat( axis, angle, degrees=False ):
+    angle = np.deg2rad( angle ) if degrees else angle
+    axis = axis.upper()
+    ret = np.eye( 4, dtype=np.float32 )
+
+    ca = np.cos( angle )
+    sa = np.sin( angle )
+
+    if (axis == "X"):
+        mat = [ [ 1.0, 0.0, 0.0 ],
+                [ 0.0,  ca, -sa ],
+                [ 0.0,  sa,  ca ] ]
+    elif (axis == "Y"):
+        mat = [ [  ca, 0.0,  sa ],
+                [ 0.0, 1.0, 0.0 ],
+                [ -sa, 0.0,  ca ] ]
+    elif (axis == "Z"):
+        mat = [ [  ca, -sa, 0.0 ],
+                [  sa,  ca, 0.0 ],
+                [ 0.0, 0.0, 1.0 ] ]
+    else:
+        mat = np.eye( 3, dtype=FLOAT_T )
+
+    ret[:3,:3 ] = np.asarray( mat, dtype=FLOAT_T )
+
+    return ret
+
+
+def genPerspProjectionFrust( h_fov, aspect, near_clip, far_clip ):
+    ymax = near_clip * np.tan( np.deg2rad( h_fov ) / 2.0 )
+    xmax = ymax * aspect
+    return genPerspProjectionPlaines( -xmax, xmax, -ymax, ymax, near_clip, far_clip )
+
+
+def genPerspProjectionPlaines( left, right, base, top, near, far ):
+    rml = right - left
+    tmb = top - base
+    fmn = far - near
+    tnr = 2.0 * near
+    return np.array( (
+        ( tnr / rml,       0.0, (right + left) / rml,              0.0),
+        (       0.0, tnr / tmb,   (top + base) / tmb,              0.0),
+               (0.0,       0.0,  -(far + near) / fmn, -far * tnr / fmn),
+        (       0.0,       0.0,                 -1.0,              0.0),
+    ), dtype=FLOAT_T )
+
+
+def genOrthoProjectionPlains( left, right, base, top, near, far ):
+    rml = right - left
+    tmb = top - base
+    fmn = far - near
+    return np.array( (
+        (2.0 / rml, 0.0, 0.0, -(right + left) / rml),
+        (0.0, 2.0 / tmb, 0.0, -(top + base) / tmb),
+        (0.0, 0.0, -2.0 / fmn, -(far - near) / fmn),
+        (0.0, 0.0, 0.0, 1.0),
+    ), dtype=FLOAT_T )
--- Python/GUI/glViewers/viewerCameras.py
+++ Python/GUI/glViewers/viewerCameras.py
@@ -10,7 +10,7 @@ from PySide2 import QtCore, QtGui, QtWidgets, QtOpenGL
 import ctypes
 from OpenGL import GL, GLU, GLUT
 
-
+from Core.math3D import genOrthoProjectionPlains, FLOAT_T
 
 SQUARES = [ x ** 2 for x in range( 9 ) ]
 
@@ -18,10 +18,12 @@ class Shaders( object ):
     dot_vtx_src = """
     # version 330 core
 
-    layout( location=0 ) in vec2 position ;
-
+    layout( location=0 ) in vec2 a_position ;
+    
+    uniform mat4 u_projection ;
+    
     void main() {
-        gl_Position = vec4( position, 0.0, 1.0 ) ;
+        gl_Position = u_projection * vec4( position, 0.0, 1.0 ) ;
     }
     """
     dot_frg_src = """
@@ -36,6 +38,9 @@ class Shaders( object ):
 
 class QGLCameraView( QtWidgets.QOpenGLWidget ):
 
+    DEFAULT_ZOOM = 1.05
+    TRUCK_SCALE  = 0.01
+
     def __init__(self, parent=None ):
         super(QGLCameraView, self).__init__( parent )
         # canvas size
@@ -47,6 +52,19 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
         self.cam_list = []
         self.num_cams = 0
 
+        # Global Camera settings
+        self.clip_nr = -100.0
+        self.clip_far = 100.0
+
+        # per camera settings for OrthoGraphic Projection
+        self._ortho_navigation = [] # should be in sceneMan
+
+        # internals for camera movement
+        self._panning = False
+        self._zooming = False
+        self._move_start = None
+        self._navigate = None
+
         # OK, for now assume we display all camera, not a selected sub-set
         self.stride_list = []
         # helper for s_in and s_out indexes
@@ -55,6 +73,7 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
         # OpenGL attrs
         self.shader = None
         self.shader_attr_locs = {}
+        self._proj_loc = -1
 
         # The Reticule (pairs for glLines)
         self._reticule = np.array(
@@ -67,13 +86,13 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
               [ 1.0, 0.0], [ 0.95, 0.0],
               [ 0.0,-1.0], [ 0.0, -0.95],
               [ 0.0, 1.0], [ 0.0,  0.95 ],
-            ], dtype=np.float32 ) # 16 Elems, 128 Bytes
+            ], dtype=FLOAT_T ) # 16 Elems, 128 Bytes
         self._reticule *= 0.99 # just smaller than NDC limits?
         self.vbo = None
         self.has_data = False
 
     def acceptNewData( self, dets, strides, ids ):
-        my_dets = np.asarray( dets[:,:2], dtype=np.float32 ) # ignore r for now
+        my_dets = np.asarray( dets[:,:2], dtype=FLOAT_T ) # ignore r for now
         self.stride_list = np.asarray( strides, dtype=np.int32 )
         packed_data = np.concatenate( (self._reticule, my_dets) )
 
@@ -94,9 +113,9 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
         self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Vertex, Shaders.dot_vtx_src  )
         self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Fragment, Shaders.dot_frg_src )
 
-        self.shader_attr_locs[ "position" ] = 0
-        GL.glEnableVertexAttribArray( self.shader_attr_locs[ "position" ] )
-        GL.glBindAttribLocation( self.shader.programId(), self.shader_attr_locs[ "position" ], "position" )
+        self.shader_attr_locs[ "a_position" ] = 0
+        GL.glEnableVertexAttribArray( self.shader_attr_locs[ "a_position" ] )
+        GL.glBindAttribLocation( self.shader.programId(), self.shader_attr_locs[ "a_position" ], "a_position" )
 
         self.shader.link()
         self.shader.bind()
@@ -107,6 +126,11 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
 
         # configure Attrib
         GL.glVertexAttribPointer( 0, 2, GL.GL_FLOAT, GL.GL_FALSE, 0, ctypes.c_void_p( 0 ) )
+
+        # Setup uniform for Camera navigation
+        self._proj_loc = self.shader.uniformLocation( "u_projection" )
+
+        self.shader.release()
 
         # Misc
         GL.glHint( GL.GL_POINT_SMOOTH_HINT, GL.GL_NICEST )
@@ -119,6 +143,9 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
         sub_w = int( width / cols )
         sub_h = int( height / rows )
         self._subwindow_sz = ( sub_w, sub_h )
+        for view in self._ortho_navigation:
+            x, y = view["locus"]
+            view["P"] = self.projectionFromNav( view["zoom"], view["width"], x, y )
 
     def paintGL( self ):
         """ Draw the cameras """
@@ -154,7 +181,12 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
                 x, y =  c*w, r*h
 
                 # setup an Orthogonal Projection
-                # Todo: correct aspect ratio
+                # Todo: correct aspect ratio.  What do we do with non-square sensors? /=(1920/2) -= [1,1280/1920] - Do in arbiter
+
+                self.shader.bind()
+
+                GL.glUniformMatrix4fv( self._proj_loc, 1, GL.GL_TRUE, self._ortho_navigation[ cam_idx ][ "P" ] )
+
                 GL.glViewport( x, y, w, h )
 
                 # ToDo: Far in the future, draw an image - texture on a quad in BG?
@@ -171,6 +203,8 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
                 # Draw reticule
                 GL.glDrawArrays( GL.GL_LINES, 0, 16 )
 
+                self.shader.release()
+
                 # Kills redrawws?
                 overlays.append( ( x+hw-45, height-y-5, "Camera {}".format( cam_idx+1 ) ) )
 
@@ -186,20 +220,158 @@ class QGLCameraView( QtWidgets.QOpenGLWidget ):
         # painter.end()
 
     def _camFromXY( self, x, y ):
-        rows, _ = self._rc
+        rows, cols = self._rc
         w, h = self._subwindow_sz
-        y = y - self._wh[ 1 ]  # top left to bottom left conversion
+        #y = y - self._wh[ 1 ]  # top left to bottom left conversion
         c = x // w
         r = y // h
-        idx = (rows * r) + c
-        if (idx > len( self.cam_list )):
+        idx = (cols * r) + c
+        if (idx >= len( self.cam_list )):
             idx = -1
         return idx
+
+    def wheelEvent( self, event ):
+        x, y = event.x(), event.y()
+        cam_idx = self._camFromXY( x, y )
+        if( cam_idx < 0 ):
+            return
+
+        cam = self._ortho_navigation[ cam_idx ]
+
+        wheel_delta = event.angleDelta().y()
+        wheel_delta /= 100.
+
+        if( wheel_delta > 0.0 ):
+            cam["zoom"] *= self.DEFAULT_ZOOM
+
+        elif( wheel_delta < 0.0 ):
+            cam["zoom"] /= self.DEFAULT_ZOOM
+
+        self.updateProjection( cam_idx )
+
+    def mousePressEvent( self, event ):
+        x, y = event.x(), event.y()
+        cam_idx = self._camFromXY( x, y )
+        if( cam_idx < 0 ):
+            return
+
+        buttons = event.buttons()
+
+        pressed_left = bool( buttons & QtCore.Qt.LeftButton )
+        pressed_right = bool( buttons & QtCore.Qt.RightButton )
+        pressed_mid = bool( buttons & QtCore.Qt.MiddleButton )
+
+        if ((pressed_left and pressed_right) or pressed_mid):
+            # trucking
+            self._panning = True
+            self._move_start = event.pos()
+            self._navigate = cam_idx
+
+        elif (pressed_right):
+            # Zooming
+            self._zooming = True
+            self._move_start = event.pos()
+            self._navigate = cam_idx
+
+    def mouseReleaseEvent( self, event ):
+        update_camera = False
+
+        if( self._panning ):
+            # complete the pan
+            update_camera = True
+            self._panning = False
+
+        elif( self._zooming ):
+            # complete the Zoom
+            update_camera = True
+            self._zooming = False
+
+        if( update_camera ):
+            self.updateProjection( self._navigate )
+
+        self._navigate = None
+
+    def mouseMoveEvent( self, event ):
+        update_camera = False
+
+        if( self._panning or self._zooming ):
+            # get the positional delta
+            hz = event.x() - self._move_start.x()
+            vt = event.y() - self._move_start.y()
+            self._move_start = event.pos()
+
+
+        if( self._panning ):
+            cam = self._ortho_navigation[ self._navigate ]
+            cam["locus"][0] += (hz * cam["zoom"]) * self.TRUCK_SCALE
+            cam["locus"][1] += (vt * cam["zoom"]) * self.TRUCK_SCALE
+
+            update_camera = True
+
+
+        elif( self._zooming ):
+            cam = self._ortho_navigation[ self._navigate ]
+            if( vt > 0.0 ):
+                cam["zoom"] *= self.DEFAULT_ZOOM
+            elif( vt < 0.0 ):
+                cam["zoom"] /= self.DEFAULT_ZOOM
+
+            update_camera = True
+
+
+        if( update_camera ):
+            self.updateProjection( self._navigate )
+
+
+    def mouseDoubleClickEvent( self, event ):
+        x, y = event.x(), event.y()
+        cam_idx = self._camFromXY( x, y )
+        if( cam_idx < 0 ):
+            return
+
+        if( event.modifiers() & QtCore.Qt.AltModifier ):
+
+            # Alt Double Click resets the camera
+            cam = self._ortho_navigation[ cam_idx ]
+            cam[ "zoom" ]  = 1.0
+            cam[ "width" ] = 2.0
+            cam[ "locus" ] = [ 0.0, 0.0 ]
+
+            self.updateProjection( cam_idx )
+        else:
+            print( self._ortho_navigation[ cam_idx ] )
+
+
+    def updateProjection( self, cam_idx ):
+        cam = self._ortho_navigation[ cam_idx ]
+        cam[ "P" ] = self.projectionFromNav( cam[ "zoom" ], cam[ "width" ], cam["locus"][0], cam["locus"][1] )
+        self.update()
 
     def _camlistChanged( self ):
         self.num_cams = len( self.cam_list )
         self._rc = self.genDimsSquare( self.num_cams )
         self._marshelStrides()
+
+        # Hack: This should be a sceneMan thing.
+        # Populate cameras with Ortho Navigation
+        self._ortho_navigation = []
+        for i in range( self.num_cams ):
+            self._ortho_navigation.append( { "zoom"  : 1.0,
+                                             "width" : 3.0,
+                                             "locus" : [ 0.0, 0.0 ],
+                                             "P"     : np.eye( 4, dtype=FLOAT_T )} )
+
+    def projectionFromNav( self, zoom, width, orig_x, orig_y  ):
+        w, h = self._subwindow_sz
+        span = width *zoom
+        extent = span / (w/h)
+
+        o_l = orig_x + span
+        o_r = orig_x - span
+        o_t = orig_y + extent
+        o_b = orig_y - extent
+
+        return genOrthoProjectionPlains( o_l, o_r, o_b, o_t, self.clip_nr, self.clip_far )
 
     def _marshelStrides( self ):
         self._strider = []
