ffa145aa8d952a837c939d0ce763a8aaf2f70232
bit-meddler <RichH@Beacon-Optical.co.uk>
2021-05-01 14:22:44 +0100

    wrote a little helper to automate calculation of strides for vertex attributes.  No actual progress on drawing, not even a triangle :(.

--- Python/Experiments/openGLviewport.py
+++ Python/Experiments/openGLviewport.py
@@ -1,11 +1,12 @@
-import ctypes
+#import ctypes
 import sys
 import numpy as np
 
 # Workaround not being in PATH
 import os, sys
 _git_root_ = os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) )
-CODE_PATH = os.path.join( _git_root_, "midget", "Python" )
+CODE_PATH = os.path.join( _git_root_, "Python" )
+print( CODE_PATH )
 sys.path.append( CODE_PATH )
 
 from Core.math3D import *
@@ -15,6 +16,9 @@ import cv2
 from PySide2 import QtCore, QtGui, QtWidgets
 from shiboken2 import VoidPtr
 from OpenGL import GL
+
+import re
+from textwrap import TextWrapper
 
 
 class OGLWindow( QtWidgets.QWidget ):
@@ -29,7 +33,95 @@ class OGLWindow( QtWidgets.QWidget ):
         self.setWindowTitle( "Testing OpenGL" )
 
 
-class Shaders( object ):
+class ShaderHelper( object ):
+    # Overload these with your shader program
+    vtx_src = None
+    frg_src = None
+    geo_src = None
+
+    # or place a path to the file / QResource
+    geo_f = None
+    frg_f = None
+    vtx_f = None
+
+    LYT_RE = r".*(?:layout)" + \
+             r"(?:\(\s*location\s*=\s*(?P<location>\d)\s*\))?" + \
+             r"\s+(?P<specifier>\w+)" + \
+             r"\s+(?P<type>[a-z]+)(?P<shape>\d*(?:x\d)?)" + \
+             r"\s*(?P<name>.*?)\s*;"
+
+    MAIN_RE = r".*void\s+main\(\).*"
+
+    LAYOUT_MATCH = re.compile( LYT_RE  )
+    MAIN_MATCH   = re.compile( MAIN_RE )
+
+    def __init__( self ):
+        # TODO: load from files
+
+        # Examine the Vertex Shader's attributes
+        attr_data = []
+        for line in self.vtx_src.splitlines():
+            if (not line):
+                continue
+
+            match = self.LAYOUT_MATCH.match( line )
+            if (match):
+                attr_data.append( match.groupdict() )
+                continue
+
+            match = self.MAIN_MATCH.match( line )
+            if (match):
+                break
+
+        # Determine the layout data
+        offset = 0
+        encountered = set()
+        layout_data = {}
+
+        for idx, attr in enumerate( attr_data ):
+            # some preemptive error checks
+            if (attr[ "name" ] in encountered):
+                print( "Warning, already processed a variable called '{}'".format( attr[ "name" ] ) )
+            encountered.add( attr[ "name" ] )
+
+            if (attr[ "location" ] in layout_data):
+                print( "Warning, already processed data in location '{}'".format( attr[ "location" ] ) )
+
+            # New in GLSL 4.0 double, dvecN, dmatN, dmatNxM
+            size = 8 if (attr[ "type" ].startswith( "d" )) else 4  # even a bool is a 32-bit value
+
+            dims = 0
+            if (attr[ "shape" ] == ""):  # Scaler
+                dims = 1
+
+            elif ("x" in attr[ "shape" ]):  # Has to be a matrix
+                N, M = map( int, attr[ "shape" ].split( "x" ) )
+                dims = N * M
+
+            else:  # either a vec or a square mat
+                N = int( attr[ "shape" ] )
+                if ("mat" in attr[ "type" ]):
+                    dims = N * N
+                else:
+                    dims = N
+
+            layout_data[ int( attr[ "location" ] ) ] = ( attr[ "name" ], dims, offset )
+            offset += dims * size
+
+        # Stash the layout data
+        self.attr_locs = {}
+
+        layout = []
+        for idx in sorted( layout_data.keys() ):
+            data = layout_data[ idx ]
+            layout.append( data )
+            self.attr_locs[ data[0] ] = idx
+
+        self.layout = tuple( layout )
+        self.line_size = offset
+
+
+class BasicShaders( ShaderHelper ):
     vtx_src = """
         # version 330
 
@@ -49,9 +141,6 @@ class Shaders( object ):
 
         }
     """
-
-    program_attrs = ( "a_position", "a_colour", "a_uvCoord" )
- 
     frg_src = """
         # version 330
 
@@ -79,7 +168,7 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
         # OpenGL setup
         self.bgcolour = [ 0.0, 0.1, 0.1, 1.0 ]
-        self.shader_attr_locs = {}
+        self._shaders = BasicShaders()
         #self.shader = QtGui.QOpenGLShaderProgram( self.context() )
 
         # canvas size
@@ -97,19 +186,19 @@ class GLVP( QtWidgets.QOpenGLWidget ):
     def _prepareResources( self ):
         #        positions         colors          texture coords
         cube = [-0.5, -0.5, 0.5,   1.0, 0.0, 0.0,  0.0, 0.0,
-                0.5, -0.5, 0.5,   0.0, 1.0, 0.0,  1.0, 0.0,
-                0.5,  0.5, 0.5,   0.0, 0.0, 1.0,  1.0, 1.0,
+                 0.5, -0.5, 0.5,   0.0, 1.0, 0.0,  1.0, 0.0,
+                 0.5,  0.5, 0.5,   0.0, 0.0, 1.0,  1.0, 1.0,
                 -0.5,  0.5, 0.5,   1.0, 1.0, 1.0,  0.0, 1.0,
 
                 -0.5, -0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
-                0.5, -0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
-                0.5,  0.5, -0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
+                 0.5, -0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
+                 0.5,  0.5, -0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
                 -0.5,  0.5, -0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
 
-                0.5, -0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
-                0.5,  0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
-                0.5,  0.5,  0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
-                0.5, -0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
+                 0.5, -0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
+                 0.5,  0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
+                 0.5,  0.5,  0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
+                 0.5, -0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
 
                 -0.5,  0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
                 -0.5, -0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
@@ -117,28 +206,31 @@ class GLVP( QtWidgets.QOpenGLWidget ):
                 -0.5,  0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
 
                 -0.5, -0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
-                0.5, -0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
-                0.5, -0.5,  0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
+                 0.5, -0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
+                 0.5, -0.5,  0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
                 -0.5, -0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
 
-                0.5,  0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
+                 0.5,  0.5, -0.5,  1.0, 0.0, 0.0,  0.0, 0.0,
                 -0.5,  0.5, -0.5,  0.0, 1.0, 0.0,  1.0, 0.0,
                 -0.5,  0.5,  0.5,  0.0, 0.0, 1.0,  1.0, 1.0,
-                0.5,  0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
+                 0.5,  0.5,  0.5,  1.0, 1.0, 1.0,  0.0, 1.0,
                 ]
+        # OK Just try a tri :|
+        cube = [-0.5, -0.5, 0.5,   1.0, 0.0, 0.0,  0.0, 0.0,
+                 0.0,  0.5, 0.5,   0.0, 1.0, 0.0,  1.0, 0.0,
+                 0.5, -0.5, 0.5,   0.0, 0.0, 1.0,  1.0, 1.0, ]
         cube = np.array( cube, dtype=np.float32 )
 
-        # ( element count, offset to first element in bytes )
-        self.stride_data = [ [3, 0], [3, 3*cube.itemsize], [2, 6*cube.itemsize] ]
-        self.line_sz = 8 * cube.itemsize
-
         indices = [
-            0,  1,  2,  2,  3,  0,
-            4,  5,  6,  6,  7,  4,
-            8,  9, 10, 10, 11,  8,
+             0,  1,  2,  2,  3,  0,
+             4,  5,  6,  6,  7,  4,
+             8,  9, 10, 10, 11,  8,
             12, 13, 14, 14, 15, 12,
             16, 17, 18, 18, 19, 16,
             20, 21, 22, 22, 23, 20,
+        ]
+        indices = [
+            0, 1, 2,
         ]
         self.indices = np.array( indices, dtype=np.uint )
 
@@ -172,22 +264,24 @@ class GLVP( QtWidgets.QOpenGLWidget ):
         self.ibo.allocate( self.indices.tobytes(), self.indices.nbytes )
         self.ibo.release()
 
-        # Load Texture Image
-        img = cv2.imread( "wood.jpg", cv2.IMREAD_COLOR )
-        rgb_img = cv2.cvtColor( img, cv2.COLOR_BGR2RGB )
-        i_h, i_w, _ = rgb_img.shape
+        # Skip texture for now, commented out in shader
+        if( False ):
+            # Load Texture Image
+            img = cv2.imread( "wood.jpg", cv2.IMREAD_COLOR )
+            rgb_img = cv2.cvtColor( img, cv2.COLOR_BGR2RGB )
+            i_h, i_w, _ = rgb_img.shape
 
-        # convert cv2 Image to QImage
-        q_img = QtGui.QImage( rgb_img.data, i_w, i_h, (3 * i_w), QtGui.QImage.Format_RGB888 )
+            # convert cv2 Image to QImage
+            q_img = QtGui.QImage( rgb_img.data, i_w, i_h, (3 * i_w), QtGui.QImage.Format_RGB888 )
 
-        # Create Texture
-        self.texture = QtGui.QOpenGLTexture( QtGui.QOpenGLTexture.Target2D ) # Target2D === GL_TEXTURE_2D
-        self.texture.create()
-        self.texture.bind()
-        self.texture.setData( q_img )
-        self.texture.setMinMagFilters( QtGui.QOpenGLTexture.Linear, QtGui.QOpenGLTexture.Linear )
-        self.texture.setWrapMode( QtGui.QOpenGLTexture.DirectionS, QtGui.QOpenGLTexture.ClampToEdge )
-        self.texture.setWrapMode( QtGui.QOpenGLTexture.DirectionT, QtGui.QOpenGLTexture.ClampToEdge )
+            # Create Texture
+            self.texture = QtGui.QOpenGLTexture( QtGui.QOpenGLTexture.Target2D ) # Target2D === GL_TEXTURE_2D
+            self.texture.create()
+            self.texture.bind()
+            self.texture.setData( q_img )
+            self.texture.setMinMagFilters( QtGui.QOpenGLTexture.Linear, QtGui.QOpenGLTexture.Linear )
+            self.texture.setWrapMode( QtGui.QOpenGLTexture.DirectionS, QtGui.QOpenGLTexture.ClampToEdge )
+            self.texture.setWrapMode( QtGui.QOpenGLTexture.DirectionT, QtGui.QOpenGLTexture.ClampToEdge )
 
         # Release the VAO Mutex Binder
         del( vaoBinder )
@@ -196,12 +290,11 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
         self.shader = QtGui.QOpenGLShaderProgram( self.context() )
 
-        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Vertex, Shaders.vtx_src  )
-        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Fragment, Shaders.frg_src )
+        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Vertex, self._shaders.vtx_src  )
+        self.shader.addShaderFromSourceCode( QtGui.QOpenGLShader.Fragment, self._shaders.frg_src )
 
-        for i, name in enumerate( Shaders.program_attrs ):
-            self.shader_attr_locs[ name ] = i
-            self.shader.bindAttributeLocation( name, i )
+        for name, location in self._shaders.attr_locs.items():
+            self.shader.bindAttributeLocation( name, location )
 
         is_linked = self.shader.link()
         if( not is_linked ):
@@ -211,33 +304,55 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
         self.rot_loc = self.shader.uniformLocation( "u_rotation" )
 
+        eye = QtGui.QMatrix4x4()
+        self.shader.setUniformValue( self.rot_loc, eye )
+
         self.shader.release()
 
     def _configureVertexAttribs( self ):
         self.vbo.bind()
         f = QtGui.QOpenGLContext.currentContext().functions()
         f.initializeOpenGLFunctions()
-        for name, (num, offset) in zip( Shaders.program_attrs, self.stride_data ):
-            print( "Setting VAP on {}".format( name ) )
-            ptr = VoidPtr( offset )
-            f.glEnableVertexAttribArray( self.shader_attr_locs[ name ] )
-            f.glVertexAttribPointer(  self.shader_attr_locs[ name ],
+        for name, num, offset in self._shaders.layout:
+            location = self._shaders.attr_locs[ name ]
+            print( "Setting VAP on '{}' @ {} with {} elems from {}b".format(
+                name, location, num, offset ) )
+            os_ptr = VoidPtr( offset )
+            f.glEnableVertexAttribArray( location )
+            f.glVertexAttribPointer(  location,
                                       num,
                                       GL.GL_FLOAT,
                                       GL.GL_FALSE,
-                                      self.line_sz,
-                                      ptr )
+                                      self._shaders.line_size,
+                                      os_ptr )
         self.vbo.release()
 
-    def getGlInfo( self, context ):
+    def getGlInfo( self, context, show_ext=False ):
         print( "Getting GL Info" )
+
         f = QtGui.QOpenGLFunctions( context )
         f.initializeOpenGLFunctions()
         ven = f.glGetString( GL.GL_VENDOR )
         ren = f.glGetString( GL.GL_RENDERER )
         ver = f.glGetString( GL.GL_VERSION )
         slv = f.glGetString( GL.GL_SHADING_LANGUAGE_VERSION )
-        info = """Vendor: {}\nRenderer: {}\nOpenGL Version: {}\nShader Version: {}""".format( ven, ren, ver, slv )
+        ext = f.glGetString( GL.GL_EXTENSIONS )
+
+        exts = ""
+        if (show_ext):
+            pad = "            "
+            w = TextWrapper( width=100, initial_indent="", subsequent_indent=pad )
+            lines = w.wrap( ext )
+            exts = "\n".join( lines )
+
+        else:
+            exts = "{} installed".format( len( ext.split() ) )
+
+        info = "Vendor: {}\nRenderer: {}\nOpenGL Version: {}\nShader Version: {}\nExtensions: {}".format(
+            ven, ren, ver, slv, exts )
+
+        # Get context Info?
+
         return info
 
     def _onClosing( self ):
@@ -265,6 +380,8 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
     # gl funs --------------------------------------------------------
     def initializeGL( self ):
+        super( GLVP, self ).initializeGL()
+
         # init GL Context
         ctx = self.context()
         ctx.aboutToBeDestroyed.connect( self._onClosing )
@@ -305,20 +422,23 @@ class GLVP( QtWidgets.QOpenGLWidget ):
         # get Context
         ctx = self.context()
         f = QtGui.QOpenGLFunctions( ctx )
-        f.initializeOpenGLFunctions()
+        #f.initializeOpenGLFunctions()
+
+        # gl Viewport
+        f.glViewport( 0, 0, self.wh[0], self.wh[1] )
 
         # attach VAO
         vaoBinder = QtGui.QOpenGLVertexArrayObject.Binder( self.vao )
         self.shader.bind()
 
-        # ??? I'm I supposed to bind these guys here?
+        # ??? am I supposed to bind these guys here?
         self.vbo.bind()
         self.ibo.bind()
 
         # Move the Cube
-        rotation = QtGui.QMatrix4x4()
-        rotation.rotate( self.rot, 1.0, 0.0, 0.0 )
-        rotation.rotate( 17.5, 0.0, 1.0, 0.0 )
+        rotation = QtGui.QMatrix4x4() # Remember these are transpose
+        rotation.rotate( self.rot, 1.0, 0.0, 0.0 ) # ang, axis
+        rotation.rotate( 17.5,     0.0, 1.0, 0.0 )
 
         self.shader.setUniformValue( self.rot_loc, rotation )
 
@@ -329,7 +449,7 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
         self.rot += 1.0
 
-        # Show em it's doing something...
+        # Show me it's doing something, even if it's not drawing anything...
         if( self.rot % 80 == 0 ):
             print( ".", end="\n" )
         else:
@@ -345,7 +465,6 @@ class GLVP( QtWidgets.QOpenGLWidget ):
 
     def resizeGL( self, width, height ):
         self.wh = ( width, height )
-        #GL.glViewport( 0, 0, width, height )
     
     
 if( __name__ == "__main__" ):
